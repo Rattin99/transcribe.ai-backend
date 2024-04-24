04303f5084deab221faffd3454d463e0f1151365
Rattin Sadman <rattin99m18@gmail.com>
2024-04-25 02:08:17 +0600

    connect with openAi

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const multer = require('multer');
+const OpenAI = require('openai');
+const openai = new OpenAI({
+    apiKey: "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"
+});
+const dotenv = require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const FormData = require('form-data');
+const axios = require('axios');
+
+
+async function transcribe(filePath,res) {
+    console.log("filepath:",filePath)
+
+    const transcription = await openai.audio.transcriptions.create({
+        file: fs.createReadStream(filePath),
+        model: "whisper-1",
+        response_format: "text",
+    });
+    
+    res.status(200).json({text: transcription})
+    
+}
 
 app.use(cors({
    origin: 'http://localhost:3000'
@@ -28,12 +51,22 @@ app.get("/", (req, res) => {
 })
 
 app.post("/upload", upload.single('file'), async (req, res) => {
-   
-    console.log(req.body)
-    res.status(200).send("File uploaded successfully");
+    
+    const filePath = path.join(__dirname, 'uploads', req.file.originalname);
+
+    transcribe(filePath,res);
+  
+
+
 })
 
 app.listen(5000, () => {
     console.log("Server started on port 5000");
 })
+
+
+
+
+
+
 
